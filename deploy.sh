7dc521a60bcc723c3b858d3268ca4577fca5f419
Peter Bellaby <bellaby1993@hotmail.co.uk>
2026-05-29 19:51:05 +0100

    move deploy script to separate file

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
-            cd ~/TrinitasApi
-            
-            git fetch origin main
-            
-            if ! git diff --quiet HEAD origin/main -- package.json; then
-              echo "Changes detected in package.json. Running npm install..."
-              git pull origin main
-              npm install --production
-            else
-              echo "No changes in package.json. Skipping npm install."
-              git pull origin main
-            fi
+cd ~/TrinitasApi
 
-            pm2 reload TrinitasApi --update-env
+git fetch origin main
+
+if ! git diff --quiet HEAD origin/main -- package.json; then
+    echo "Changes detected in package.json. Running npm install..."
+    git pull origin main
+    npm install --production
+else
+    echo "No changes in package.json. Skipping npm install."
+    git pull origin main
+fi
+
+pm2 reload trinitas-api --update-env
